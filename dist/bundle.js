f158d67a0a02d4f82909ef61460288322b2b274c
Manolo Salsas <manolez@gmail.com>
2016-03-24 09:04:12 +0100

    Add new bundled version and libs and rename project to react-svg-components

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,8 +1,5 @@
-"use strict";
-
-var React = require('react');
-var PropTypes = React.PropTypes;
-var Component = React.Component;
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.react_svg = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
     value: true
@@ -15,6 +12,10 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var React = require('react');
+var PropTypes = React.PropTypes;
+var Component = React.Component;
 
 var Arc = function (_Component) {
     _inherits(Arc, _Component);
@@ -49,16 +50,16 @@ var Arc = function (_Component) {
     }
 
     _createClass(Arc, [{
-        key: "render",
+        key: 'render',
         value: function render() {
             return React.createElement(
-                "svg",
-                { width: this.props.radius * 2 + this.props.strokeWidth * 2, height: this.props.radius * 2 + this.props.strokeWidth * 2, xmlns: "http://www.w3.org/2000/svg" },
+                'svg',
+                { width: this.props.radius * 2 + this.props.strokeWidth * 2, height: this.props.radius * 2 + this.props.strokeWidth * 2, xmlns: 'http://www.w3.org/2000/svg' },
                 React.createElement(
-                    "g",
+                    'g',
                     null,
-                    React.createElement("path", { d: this.describeArc(this.props.radius, this.props.strokeWidth, this.props.degreesOffset, this.props.degrees), className: "msalsas-arc",
-                        fill: "none", strokeWidth: this.props.strokeWidth, stroke: this.props.strokeColor })
+                    React.createElement('path', { d: this.describeArc(this.props.radius, this.props.strokeWidth, this.props.degreesOffset, this.props.degrees), className: 'msalsas-arc',
+                        fill: 'none', strokeWidth: this.props.strokeWidth, stroke: this.props.strokeColor })
                 )
             );
         }
@@ -82,3 +83,6 @@ Arc.defaultProps = {
     strokeColor: '#fff'
 };
 exports.default = Arc;
+
+},{"react":"react"}]},{},[1])(1)
+});
