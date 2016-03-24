89b138c38b5b1086a91442f1e305edc263739dea
Manolo Salsas <manolez@gmail.com>
2016-03-24 10:57:19 +0100

    Rename urls in package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,3 @@
-/* Add manually these lines to bundle.js once built:
-var React = require('react');
-var PropTypes = React.PropTypes;
-var Component = React.Component;
-
-and remove the first and last line
-*/
-
 var gulp = require('gulp');
 var browserify = require('browserify');
 var babelify = require('babelify');
